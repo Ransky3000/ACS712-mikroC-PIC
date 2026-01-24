ccb94c4485753172a4066f3223ec516160a882d8
Ranian J. Rulona <ranian.rulona14449@gmail.com>
2026-01-24 14:11:18 +0800

    feat: Create initial files for the Software UART library, including header, source, and a test main.

---
+++ MPLAB Compatible/Library Dev/Software_UART.X/Software_UART.c
@@ -0,0 +1,8 @@
+/*
+ * File:   Software_UART.c
+ * Author: USER
+ *
+ * Created on January 24, 2026, 2:05 PM
+ */
+
+
---
+++ MPLAB Compatible/Library Dev/Software_UART.X/Test_main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   Test_main.c
+ * Author: USER
+ *
+ * Created on January 24, 2026, 2:05 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
