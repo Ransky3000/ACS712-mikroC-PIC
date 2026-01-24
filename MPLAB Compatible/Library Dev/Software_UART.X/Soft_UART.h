e6e809c34e400c72f60e455d7f4c9c7247590e04
Ranian J. Rulona <ranian.rulona14449@gmail.com>
2026-01-24 17:37:39 +0800

    feat: Implement Non-Blocking Software UART (Timer2)

--- MPLAB Compatible/Library Dev/Software_UART.X/Soft_UART.h
+++ MPLAB Compatible/Library Dev/Software_UART.X/Soft_UART.h
@@ -1,78 +1,25 @@
-/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
- * and any derivatives exclusively with Microchip products. 
- * 
- * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
- * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
- * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
- * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
- * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
- *
- * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
- * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
- * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
- * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
- * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
- * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
- * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
- *
- * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
- * TERMS. 
- */
+/* File: Soft_UART.h */
+#ifndef SOFT_UART_H
+#define SOFT_UART_H
 
-/* 
- * File:   
- * Author: 
- * Comments:
- * Revision history: 
- */
+#include <xc.h>
 
-// This is a guard condition so that contents of this file are not included
-// more than once.  
-#ifndef XC_HEADER_TEMPLATE_H
-#define	XC_HEADER_TEMPLATE_H
+// Initialize Soft UART (Non-Blocking)
+// port: Address of PORT register (e.g. &PORTB)
+// rx_pin: bit number (0-7)
+// tx_pin: bit number (0-7)
+// baud_rate: e.g. 9600
+// inverted: 0=Normal, 1=Inverted
+void Soft_UART_Init(volatile unsigned char *port, unsigned char rx_pin, unsigned char tx_pin, unsigned long baud_rate, unsigned char inverted);
 
-#include <xc.h> // include processor files - each processor file is guarded.  
+// Read byte (Non-blocking)
+// Returns data. Sets *error to 0 (Success), 1 (No Data), 2 (Overflow)
+char Soft_UART_Read(char *error);
 
-// TODO Insert appropriate #include <>
+// Write byte (Non-blocking / Buffered)
+void Soft_UART_Write(char udata);
 
-// TODO Insert C++ class definitions if appropriate
+// Reset State
+void Soft_UART_Break();
 
-// TODO Insert declarations
-
-// Comment a function and leverage automatic documentation with slash star star
-/**
-    <p><b>Function prototype:</b></p>
-  
-    <p><b>Summary:</b></p>
-
-    <p><b>Description:</b></p>
-
-    <p><b>Precondition:</b></p>
-
-    <p><b>Parameters:</b></p>
-
-    <p><b>Returns:</b></p>
-
-    <p><b>Example:</b></p>
-    <code>
- 
-    </code>
-
-    <p><b>Remarks:</b></p>
- */
-// TODO Insert declarations or function prototypes (right here) to leverage 
-// live documentation
-
-#ifdef	__cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
-    // TODO If C++ is being used, regular C code needs function names to have C 
-    // linkage so the functions can be used by the c code. 
-
-#ifdef	__cplusplus
-}
-#endif /* __cplusplus */
-
-#endif	/* XC_HEADER_TEMPLATE_H */
-
+#endif
--- MPLAB Compatible/Library Dev/Software_UART.X/Test_main.c
+++ MPLAB Compatible/Library Dev/Software_UART.X/Test_main.c
@@ -1,13 +1,44 @@
-/*
- * File:   Test_main.c
- * Author: USER
- *
- * Created on January 24, 2026, 2:05 PM
- */
-
+/* File: Test_main.c */
+#pragma config FOSC = INTOSCIO
+#pragma config WDTE = OFF
+#pragma config PWRTE = ON
+#pragma config MCLRE = ON
+#pragma config BOREN = OFF
+#pragma config LVP = OFF
+#pragma config CPD = OFF
+#pragma config WRT = OFF
+#pragma config CCPMX = RB0
+#pragma config CP = OFF
+#pragma config FCMEN = OFF
+#pragma config IESO = OFF
 
 #include <xc.h>
+#include "Soft_UART.h"
 
-void main(void) {
-    return;
+#define _XTAL_FREQ 8000000
+
+void main() {
+    OSCCON = 0x70; // 8MHz
+    ANSEL = 0;     // Digital
+    
+    // Init Soft UART
+    // PORTB, RX=RB4, TX=RB1, 9600 Baud, Normal
+    Soft_UART_Init(&PORTB, 6, 7, 9600, 0); 
+    
+    while(1) {
+        Soft_UART_Write('H');
+        Soft_UART_Write('i');
+        Soft_UART_Write('\r');
+        Soft_UART_Write('\n');
+        
+        __delay_ms(300);
+        
+        // Check Echo
+        char err;
+        char c = Soft_UART_Read(&err);
+        if (err == 0) {
+            Soft_UART_Write('E'); // Echo marker
+            Soft_UART_Write(c);
+        }
+    }
 }
